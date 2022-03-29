b9cdbb9794d860d5a73ea8b025216b2b3db5215b
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-03-29 07:46:38 -0600

    Suma y resta de Matrices tipo Fraccion

--- main.cpp
+++ main.cpp
@@ -1,29 +1,196 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
-//Aqui va la clase Fraccion
+class Fraccion{
+private:
+    int numerador;
+    int denominador;
+    void verificaDatos(void);
+public:
+    Fraccion(void);
+    Fraccion(int numerador, int denominador);
+    ~Fraccion(void);
+    void pideleAlUsuarioTusDatos(void);
+    void muestraTusDatos(void);
+    int dameTuNumerador(void);
+    void modificaTuNumerador(int numerador);
+    int dameTuDenominador(void);
+    void modificaTuDenominador(int denominador);
+    void modificaTusDatos(int numerador, int denominador);
+};
+
+Fraccion suma(Fraccion A, Fraccion B);
+Fraccion resta(Fraccion A, Fraccion B);
+Fraccion multiplica(Fraccion A, Fraccion B);
+Fraccion divide(Fraccion A, Fraccion B);
+
+void Fraccion::verificaDatos(void){
+    if(this->denominador < 0){
+         this->denominador *= -1;
+         this->numerador *= -1;
+    }
+    if(this->denominador == 0){
+        cout<<"Error! Fraccion indeterminada... =("<<endl<<endl;
+        exit(-1);
+    }
+}
+Fraccion::Fraccion(void){
+    Fraccion::numerador = 0;
+    Fraccion::denominador = 1;
+}
+Fraccion::Fraccion(int numerador, int denominador){
+    Fraccion::numerador = numerador;
+    Fraccion::denominador = denominador;
+    Fraccion::verificaDatos();
+}
+Fraccion::~Fraccion(void){
+}
+void Fraccion::pideleAlUsuarioTusDatos(void){
+    cout<<"Dame mi numerador ";
+    cin>>this->numerador;
+    cout<<"Dame mi denominador ";
+    cin>>this->denominador;
+    this->verificaDatos();
+}
+void Fraccion::muestraTusDatos(void){
+    cout<<this->numerador<<"/"<<this->denominador;
+}
+int Fraccion::dameTuNumerador(void){
+    return this->numerador;
+}
+void Fraccion::modificaTuNumerador(int numerador){
+    this->numerador = numerador;
+}
+int Fraccion::dameTuDenominador(void){
+    return this->denominador;
+}
+void Fraccion::modificaTuDenominador(int denominador){
+    this->denominador = denominador;
+    this->verificaDatos();
+}
+void Fraccion::modificaTusDatos(int numerador, int denominador){
+    this->numerador = numerador;
+    this->denominador = denominador;
+    this->verificaDatos();
+}
+
+
+
+Fraccion suma(Fraccion A, Fraccion B){
+    Fraccion C;
+    C.modificaTuNumerador(
+        A.dameTuNumerador()*B.dameTuDenominador()
+        +
+        B.dameTuNumerador()*A.dameTuDenominador()
+    );
+    C.modificaTuDenominador(
+        A.dameTuDenominador()*B.dameTuDenominador()
+    );
+    return C;
+}
+Fraccion resta(Fraccion A, Fraccion B){
+    Fraccion D;
+    D.modificaTuNumerador(
+        A.dameTuNumerador()*B.dameTuDenominador()
+        -
+        B.dameTuNumerador()*A.dameTuDenominador()
+    );
+    D.modificaTuDenominador(
+        A.dameTuDenominador()*B.dameTuDenominador()
+    );
+    return D;
+}
+Fraccion multiplica(Fraccion A, Fraccion B){
+    Fraccion E;
+    E.modificaTuNumerador(
+        A.dameTuNumerador()*B.dameTuNumerador()
+    );
+    E.modificaTuDenominador(
+        A.dameTuDenominador()*B.dameTuDenominador()
+    );
+    return E;
+}
+Fraccion divide(Fraccion A, Fraccion B){
+    Fraccion F;
+    F.modificaTuNumerador(
+        A.dameTuNumerador()*B.dameTuDenominador()
+    );
+    F.modificaTuDenominador(
+        A.dameTuDenominador()*B.dameTuNumerador()
+    );
+    return F;
+}
 
 int main(void){
-    Fraccion A;
-    Fraccion F(8,9);
-    A.muestraTusDatos();cout<<endl;
+    Fraccion A[5][5], B[5][5], C[5][5], D[5][5];
+    int m,n,i,j;
 
-    F.muestraTusDatos();cout<<endl;
+    //1. Pide dato(s)
+    cout<<"Ingresa m ";cin>>m;
+    cout<<"Ingresa n ";cin>>n;
 
-    F.pideleAlUsuarioTusDatos();
-    F.muestraTusDatos();cout<<endl;
+    cout<<endl<<"Ingresa A"<<endl;
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            cout<<"Imgresa A["<<i<<"]["<<j<<"]"<<endl;
+            A[i][j].pideleAlUsuarioTusDatos();
+        }
+    }
 
-    F.modificaTuDenominador(3);
-    F.muestraTusDatos();cout<<endl;
+    cout<<endl<<"Ingresa B"<<endl;
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            cout<<"Imgresa A["<<i<<"]["<<j<<"]"<<endl;
+            B[i][j].pideleAlUsuarioTusDatos();
+        }
+    }
 
-    F.modificaTuNumerador(4);
-    F.muestraTusDatos();cout<<endl;
+    //2. Calcula
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            C[i][j] = suma(A[i][j], B[i][j]);
+        }
+    }
 
-    F.modificaTusDatos(5,6);
-    F.muestraTusDatos();cout<<endl;
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            D[i][j] = resta(A[i][j], B[i][j]);
+        }
+    }
 
-    cout<<F.dameTuNumerador()<<endl;
-    cout<<F.dameTuDenominador()<<endl;
+    //3.Muestra resultado(s)
+    cout<<endl<<"A"<<endl;
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            A[i][j].muestraTusDatos();cout<<"\t";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl<<"B"<<endl;
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            B[i][j].muestraTusDatos();cout<<"\t";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl<<"C"<<endl;
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            C[i][j].muestraTusDatos();cout<<"\t";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl<<"D"<<endl;
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            D[i][j].muestraTusDatos();cout<<"\t";
+        }
+        cout<<endl;
+    }
 
     return 0;
 }
