01539d10613bda1407171b162a116c7b00321d7b
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-03-31 07:54:07 -0600

    Tarea: Clase Rectangulo

--- main.cpp
+++ main.cpp
@@ -1,8 +1,332 @@
 #include <iostream>
-#include <math.h>
+#include <stdlib.h>
 using namespace std;
 
-//Aqui va la clase Circulo
+//Aqui va la clase Rectangulo
+
+int main(void){
+    //Pruebas para constructores
+    Rectangulo R1, R2(-2,4), R3(2,-4), R4(-2,-4),
+            R5(2,0), R6(0,4), R7(0,0), R8(2,4);
+
+    cout<<"R1";cout<<endl;
+    R1.muestraTusDatos();
+    cout<<endl;
+
+    cout<<"R2(-2,4)";cout<<endl;
+    R2.muestraTusDatos();
+    cout<<endl;
+
+    cout<<"R3(2,-4)";cout<<endl;
+    R3.muestraTusDatos();
+    cout<<endl;
+
+    cout<<"R4(-2,-4)";cout<<endl;
+    R4.muestraTusDatos();
+    cout<<endl;
+
+    cout<<"R5(2,0)";cout<<endl;
+    R5.muestraTusDatos();
+    cout<<endl;
+
+    cout<<"R6(0,4)";cout<<endl;
+    R6.muestraTusDatos();
+    cout<<endl;
+
+    cout<<"R7(0,0)";cout<<endl;
+    R7.muestraTusDatos();
+    cout<<endl;
+
+    cout<<"R8(2,4)";cout<<endl;
+    R8.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    //Pruebas para pideleAlUsuarioTusDatos()
+    cout<<"R1 (Ingresa -2 y 4)";cout<<endl;
+    R1.muestraTusDatos();
+    cout<<endl;
+    R1.pideleAlUsuarioTusDatos();
+    cout<<"R1";cout<<endl;
+    R1.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<"R2 (Ingresa 2 y -4)";cout<<endl;
+    R2.muestraTusDatos();
+    cout<<endl;
+    R2.pideleAlUsuarioTusDatos();
+    cout<<"R2";cout<<endl;
+    R2.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<"R3 (Ingresa -2 y -4)";cout<<endl;
+    R3.muestraTusDatos();
+    cout<<endl;
+    R3.pideleAlUsuarioTusDatos();
+    cout<<"R3";cout<<endl;
+    R3.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<"R4 (Ingresa 2 y 0)";cout<<endl;
+    R4.muestraTusDatos();
+    cout<<endl;
+    R4.pideleAlUsuarioTusDatos();
+    cout<<"R4";cout<<endl;
+    R4.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<"R5 (Ingresa 0 y 4)";cout<<endl;
+    R5.muestraTusDatos();
+    cout<<endl;
+    R5.pideleAlUsuarioTusDatos();
+    cout<<"R5";cout<<endl;
+    R5.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<"R6 (Ingresa 0 y 0)";cout<<endl;
+    R6.muestraTusDatos();
+    cout<<endl;
+    R6.pideleAlUsuarioTusDatos();
+    cout<<"R6";cout<<endl;
+    R6.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<"R7 (Ingresa 2 y 4)";cout<<endl;
+    R7.muestraTusDatos();
+    cout<<endl;
+    R7.pideleAlUsuarioTusDatos();
+    cout<<"R7";cout<<endl;
+    R7.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    //Pruebas para dameTuBase(), dameTuAltura()
+    //dameTuArea() y dameTuPerimetro()
+    cout<<"Pruebas para metodos con retorno de R7."<<endl;
+
+    cout<<R7.dameTuBase()<<endl
+        <<R7.dameTuAltura()<<endl
+        <<R7.dameTuArea()<<endl
+        <<R7.dameTuPerimetro()<<endl
+    ;
+    system("pause");
+    system("cls");
+
+    //Pruebas para modificaTusDatos
+    cout<<"Prueba para modificaTusDatos(-2,4)"<<endl;
+    cout<<"R1";cout<<endl;
+    R1.muestraTusDatos();
+    cout<<endl;
+
+    R1.modificaTusDatos(-2,4);
+
+    cout<<"R1";cout<<endl;
+    R1.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<"Prueba para modificaTusDatos(2,-4)"<<endl;
+    cout<<"R1";cout<<endl;
+    R1.muestraTusDatos();
+    cout<<endl;
+
+    R1.modificaTusDatos(2,-4);
+
+    cout<<"R1";cout<<endl;
+    R1.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<"Prueba para modificaTusDatos(-2,-4)"<<endl;
+    cout<<"R2";cout<<endl;
+    R2.muestraTusDatos();
+    cout<<endl;
+
+    R2.modificaTusDatos(-2,-4);
+
+    cout<<"R2";cout<<endl;
+    R2.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<"Prueba para modificaTusDatos(2,0)"<<endl;
+    cout<<"R2";cout<<endl;
+    R2.muestraTusDatos();
+    cout<<endl;
+
+    R2.modificaTusDatos(2,0);
+
+    cout<<"R2";cout<<endl;
+    R2.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<"Prueba para modificaTusDatos(0,4)"<<endl;
+    cout<<"R3";cout<<endl;
+    R3.muestraTusDatos();
+    cout<<endl;
+
+    R3.modificaTusDatos(0,4);
+
+    cout<<"R3";cout<<endl;
+    R3.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<"Prueba para modificaTusDatos(0,0)"<<endl;
+    cout<<"R3";cout<<endl;
+    R3.muestraTusDatos();
+    cout<<endl;
+
+    R3.modificaTusDatos(0,0);
+
+    cout<<"R3";cout<<endl;
+    R3.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<"Prueba para modificaTusDatos(2,4)"<<endl;
+    cout<<"R6";cout<<endl;
+    R6.muestraTusDatos();
+    cout<<endl;
+
+    R6.modificaTusDatos(2,4);
+
+    cout<<"R6";cout<<endl;
+    R6.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+
+    //Pruebas para modificaTuBase() y/o modificaTuAltura()
+    cout<<"Prueba para modificaTuBase() con -2 (altura no tiene 0)"<<endl;
+    cout<<"R6";cout<<endl;
+    R6.muestraTusDatos();
+    cout<<endl;
+
+    R6.modificaTuBase(-2);
+
+    cout<<"R6";cout<<endl;
+    R6.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<"Prueba para modificaTuAltura() con -4 (base no tiene 0)"<<endl;
+    cout<<"R7";cout<<endl;
+    R7.muestraTusDatos();
+    cout<<endl;
+
+    R7.modificaTuAltura(-4);
+
+    cout<<"R7";cout<<endl;
+    R7.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<"Prueba para modificaTuBase() con 2 (altura ya tiene 0)"<<endl;
+    cout<<"R6";cout<<endl;
+    R6.muestraTusDatos();
+    cout<<endl;
+
+    R6.modificaTuBase(2);
+
+    cout<<"R6";cout<<endl;
+    R6.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<"Prueba para modificaTuAltura() con 4 (base ya tiene 0)"<<endl;
+    cout<<"R7";cout<<endl;
+    R7.muestraTusDatos();
+    cout<<endl;
+
+    R7.modificaTuAltura(4);
+
+    cout<<"R7";cout<<endl;
+    R7.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<"Prueba para modificaTuBase() con 3 (altura no tiene 0)"<<endl;
+    cout<<"R8";cout<<endl;
+    R8.muestraTusDatos();
+    cout<<endl;
+
+    R8.modificaTuBase(3);
+
+    cout<<"R8";cout<<endl;
+    R8.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<"Prueba para modificaTuAltura() con 5 (base no tiene 0)"<<endl;
+    cout<<"R8";cout<<endl;
+    R8.muestraTusDatos();
+    cout<<endl;
+
+    R8.modificaTuAltura(5);
+
+    cout<<"R8";cout<<endl;
+    R8.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<"Prueba para modificaTuBase() con 0 (altura no tiene 0)"<<endl;
+    R7.modificaTusDatos(3,5);
+    cout<<"R7";cout<<endl;
+    R7.muestraTusDatos();
+    cout<<endl;
+
+    R7.modificaTuBase(0);
+
+    cout<<"R7";cout<<endl;
+    R7.muestraTusDatos();
+    cout<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<"Prueba para modificaTuAltura() con 0 (base no tiene 0)"<<endl;
+    cout<<"R8";cout<<endl;
+    R8.muestraTusDatos();
+    cout<<endl;
+
+    R8.modificaTuAltura(0);
+
+    cout<<"R8";cout<<endl;
+    R8.muestraTusDatos();
+    cout<<endl;
+
+    return 0;
+}
+
+
 
 int main(void){
     //Pruebas para constructores
