9c389dc1de9b8a518804f6a65d987e6e78d4c01b
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-05-02 08:19:17 -0500

    Pruebas para clase Profesor

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
-#include "Empleado.h"
+#include "Profesor.h"
 using namespace std;
 
 int main(void){
@@ -8,37 +8,37 @@ int main(void){
     Hora H(12,34,56);
     Evento E(13,8,2001,7,8,9);
     //Pruebas de constructores y muestraTusDatos()
-    Empleado E1;
-    Empleado E2("Juan","Perez","Lopez","m",1.82,E,2,4967.6,"Intendente");
-    Empleado E3("Mario","Elmada","Juarez","masc.",1.76,F,H,3,5678.9,"General");
-    Empleado E4("Laura","Zuniga","Ruiz","Fem.",1.86,29,4,2004,8,54,32,4,49876.54,"Gerente");
+    Profesor P1;
+    Profesor P2("Juan","Perez","Lopez","m",1.82,E,2,4967.6,"Profe",1,4,"Plectronica");
+    Profesor P3("Mario","Almada","Juarez","masc.",1.76,F,H,3,5678.9,"Teacher",2,9,"Humanidades");
+    Profesor P4("Laura","Zuniga","Ruiz","Fem.",1.86,29,4,2004,8,54,32,4,49876.54,"Miss",3,12.5,"Fisica");
 
-    cout<<"E1"<<endl;E1.muestraTusDatos();cout<<endl;
-    cout<<"E2"<<endl;E2.muestraTusDatos();cout<<endl;
-    cout<<"E3"<<endl;E3.muestraTusDatos();cout<<endl;
-    cout<<"E4"<<endl;E4.muestraTusDatos();cout<<endl;
+    cout<<"P1"<<endl;P1.muestraTusDatos();cout<<endl;
+    cout<<"P2"<<endl;P2.muestraTusDatos();cout<<endl;
+    cout<<"P3"<<endl;P3.muestraTusDatos();cout<<endl;
+    cout<<"P4"<<endl;P4.muestraTusDatos();cout<<endl;
     system("pause");
     system("cls");
 
-    //Prueba de pideleElUsuarioTusDatos()
-    cout<<"E1"<<endl;E1.muestraTusDatos();cout<<endl;
+    //Prueba de pidelePlUsuarioTusDatos()
+    cout<<"P1"<<endl;P1.muestraTusDatos();cout<<endl;
 
-    cout<<"E1"<<endl;E1.pideleAlUsuarioTusDatos();
+    cout<<"P1"<<endl;P1.pideleAlUsuarioTusDatos();
 
-    cout<<"E1"<<endl;E1.muestraTusDatos();cout<<endl;
+    cout<<"P1"<<endl;P1.muestraTusDatos();cout<<endl;
     system("pause");
     system("cls");
 
     //Pruebas de dameTuNumero(),dameTuSueldo(),dameTuPuesto()
     //modificaTuNumero(),modificaTuSueldo() y modificaTuPuesto()
-    cout<<"E1"<<endl;E1.muestraTusDatos();cout<<endl;
-    cout<<"E2"<<endl;E2.muestraTusDatos();cout<<endl;
+    cout<<"P1"<<endl;P1.muestraTusDatos();cout<<endl;
+    cout<<"P2"<<endl;P2.muestraTusDatos();cout<<endl;
 
-    E2.modificaTuNumero(E1.dameTuNumero());
-    E2.modificaTuSueldo(E1.dameTuSueldo());
-    E2.modificaTuPuesto(E1.dameTuPuesto());
+    P2.modificaTuGrupos(P1.dameTuGrupos());
+    P2.modificaTuCarga(P1.dameTuCarga());
+    P2.modificaTuAcademia(P1.dameTuAcademia());
 
-    cout<<"E2"<<endl;E1.muestraTusDatos();cout<<endl;E1.dameTuEstatura();
+    cout<<"P2"<<endl;P1.muestraTusDatos();cout<<endl;P1.dameTuEstatura();
 
     return 0;
 }
